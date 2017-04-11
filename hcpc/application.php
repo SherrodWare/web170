5562e2c757f61ac38ad0ba09cf033af998c1dc06
SherrodWare <kungskii88@gmail.com>
2017-04-11 16:33:54 -0700

    Initial Commit

---
+++ hcpc/application.php
@@ -0,0 +1,26 @@
+<?php include 'includes/top2.php';?>
+<link rel="stylesheet" href="css/nav.css"/>
+<!-- START LEFT Column -->
+<article>
+   <h2>We Need To Gather Information From You About Your Health</h2>
+<!--<iframe height="600" width="100%" src="https://www.schedulicity.com/scheduling/TTSFAE"></iframe>-->
+   <a href="images/appimg.jpg" data-lightbox="HCPC" data-title="Owner">
+                 <img src="images/appimg.jpg">
+  </a>
+
+</article>
+ 
+<!-- END LEFT Column --> 
+  
+<!-- START  Aside RIGHT Column -->
+<aside>
+  <h2>Fill out the Application Below Please</h2>
+  <?php
+       
+        include 'includes/multiple.php';
+
+	?>
+   
+</aside>
+<!-- END Aside RIGHT Column -->
+<?php include ("includes/bottom.php");?>
---
+++ hcpc/includes/bottom2.php
@@ -0,0 +1,17 @@
+<footer>
+  <p>`We take pride in our patients</p>
+  <p><a href="disclaimer.html">~Disclaimer~</a><a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS</a> and <a href="http://validator.w3.org/check/referer" target="_blank">~Valid HTML5</a> ~ Website by Sherrod Ware, copyright 2017</p>
+</footer>
+<script>
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+</script>
+</body>
+</html>
