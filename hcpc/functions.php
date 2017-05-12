88f82a5fdbae2b2e52cb2de8ea11fccdd9872478
SherrodWare <kungskii88@gmail.com>
2017-05-11 18:34:54 -0700

    added functions.php

---
+++ hcpc/functions.php
@@ -0,0 +1,10 @@
+<?php
+
+ /* Theme Name: Web 2017 HCPC 
+Theme URI: https://github.com/SherrodWare/web170/tree/master/hcpc
+Author: Sherrod Ware
+Author URI: http://sherrodware.com
+Description: This is my HCPC project for Web 170 WordPress
+section 01 spring 2017
+Version 42.0
+*/
