28ed805de3c30cbceb341759f8c2e24c24796cef
SherrodWare <kungskii88@gmail.com>
2017-05-23 16:21:15 -0700

    header.php (navigation) has been committed
    navigation bar is not working still.  The sub menu is wrong!!!

--- hcpc/header.php
+++ hcpc/header.php
@@ -5,7 +5,7 @@
     <title>HC PROFRESSIONAL CARE</title>
     <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/blueberry.css" />
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" />
-    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/nav.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/style.css" />
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
     <script src="<?php bloginfo('template_directory'); ?>/js/jquery.blueberry.js"></script>
@@ -17,16 +17,18 @@
         <img id= "logo" src="<?php bloginfo('template_directory'); ?>/images/websitelogomini.jpg">
         <h1>Welcome TO HCPC</h1>
         <h2>"EXPERT CARE WHEN YOU NEED IT MOST"</h2>
-        <!-- This is my unordered list for navigation -->
-        <nav id="cssmenu">
+        <!-- This is my unordered list for navigation -->   
+     <?php wp_nav_menu(array( 'theme_loction' => 'main-menu',  'container' => 'nav', 'container_id' => 'cssmenu','container_class', 'menu-main-container' ));?>
+<!--
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About Us</a></li>
-    <!--                    <li><a href="services.html">Services</a></li>-->
+                        <li><a href="services.html">Services</a></li>
                 <li><a href="caregivers.html">Caregivers</a></li>
               <li><a href="application.php">Application</a></li>
                 <li class="active"><a href="gallery.php">Gallery</a></li>
                 <li><a href="contactus.php">Contact Us</a></li>
             </ul> 
-        </nav>
+-->
+        
     </header>
