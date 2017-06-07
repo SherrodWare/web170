ba0cbf2e35a9c1bd5e958060763b35c9dab05c9f
SherrodWare <kungskii88@gmail.com>
2017-06-06 18:51:57 -0700

    committed page.php to show my custom field

--- hcpc/page.php
+++ hcpc/page.php
@@ -1,7 +1,4 @@
 <?php get_header(); ?>
-<? 
-$routePrefix = '/test/page/';
-?>
 
 
 
@@ -12,9 +9,45 @@ $routePrefix = '/test/page/';
 		<?php endwhile; endif; ?>
 		<small>page.php</small>
 	</div>
+
+<?php if (get_post_meta($post->ID, 'banner', true)) { // begin check for spotlight ?>
+
+    <!-- Begin Spotlight -->
+    <div id="spotlight">
+    <img id="spotlight-main-image" src="<?php echo get_post_meta($post->ID, 'banner', true); ?>" alt="Main Spotlight Image" width="600" height="340" />
+    <blockquote id="spotlight-main-quote">
+    <?php quotescollection_quote(); ?>
+    </blockquote>
+    </div>
+    <!-- End Spotlight -->
+
+<?php } // end check for spotlight ?>
+
+<!-- Begin Content -->
+<div id="content">
     
-<?php include "footer.php" ?>
-<?php wp_footer() ?>
+    <!-- Begin Text -->
+    <div id="text">  
+         
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article id="page-<?php the_ID(); ?>">
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+        </article>
+        <?php endwhile; endif; ?>
+    </div>
+    <!-- End Text -->
+    
+    <?php get_sidebar(); ?>
+    
+</div>
+<!-- End Content -->
+
+<?php get_footer(); ?>
+<small>page.php</small>
+    
+
+
 
 	
 
