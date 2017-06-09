77775c1fd897764f257a513c45df2dee34b9a419
SherrodWare <kungskii88@gmail.com>
2017-06-08 17:42:50 -0700

    committed page.php. quote is visable

--- hcpc/page.php
+++ hcpc/page.php
@@ -1,28 +1,5 @@
 <?php get_header(); ?>
 
-
-
-	<div>
-		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			<h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
-			<?php the_content() ?>
-		<?php endwhile; endif; ?>
-		<small>page.php</small>
-	</div>
-
-<?php if (get_post_meta($post->ID, 'banner', true)) { // begin check for spotlight ?>
-
-    <!-- Begin Spotlight -->
-    <div id="spotlight">
-    <img id="spotlight-main-image" src="<?php echo get_post_meta($post->ID, 'banner', true); ?>" alt="Main Spotlight Image" width="600" height="340" />
-    <blockquote id="spotlight-main-quote">
-    <?php quotescollection_quote(); ?>
-    </blockquote>
-    </div>
-    <!-- End Spotlight -->
-
-<?php } // end check for spotlight ?>
-
 <!-- Begin Content -->
 <div id="content">
     
@@ -42,6 +19,29 @@
     
 </div>
 <!-- End Content -->
+
+	<!--<div>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h2>
+			<?php the_content() ?>
+		<?php endwhile; endif; ?>
+		<small>page.php</small>
+	</div>-->
+
+
+<?php if (get_post_meta($post->ID, 'banner', true)) { // begin check for spotlight ?>
+
+    <!-- Begin Spotlight -->
+    <div id="spotlight">
+    <img id="spotlight-main-image" src="<?php echo get_post_meta($post->ID, 'banner', true); ?>" alt="Main Spotlight Image" width="600" height="340" />
+    <blockquote id="spotlight-main-quote">
+    <?php quotescollection_quote(); ?>
+    </blockquote>
+    </div>
+    <!-- End Spotlight -->
+
+<?php } // end check for spotlight ?>
+
 
 <?php get_footer(); ?>
 <small>page.php</small>
